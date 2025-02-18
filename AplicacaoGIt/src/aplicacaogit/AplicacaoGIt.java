e8ce1a2fe73306ec3c3a54c779eae6ecdc1f68dc
Miguel Sousa <2300710@fortecpraiagrande.com.br>
2025-02-18 08:10:06 -0300

    Adicionado a funcao soma

--- AplicacaoGIt/src/aplicacaogit/AplicacaoGIt.java
+++ AplicacaoGIt/src/aplicacaogit/AplicacaoGIt.java
@@ -1,10 +1,16 @@
 package aplicacaogit;
-
+import java.util.Scanner;
 public class AplicacaoGIt {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
         System.out.println("Meu primeiro programa "
                 + "versionado com git e hospedado no GitHub!");
+        System.out.println("Digite o primeiro valor: ");
+        int num1 = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        int num2 = scan.nextInt();
+        System.out.println("A soma é: " + (num1+num2));
     }
     
 }
