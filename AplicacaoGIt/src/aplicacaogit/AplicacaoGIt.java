a8760968ff33534dea3fd3c8ddd5a2326d60e2a4
Miguel Sousa <2300710@fortecpraiagrande.com.br>
2025-02-18 07:57:07 -0300

    Primeiro Commit

---
+++ AplicacaoGIt/src/aplicacaogit/AplicacaoGIt.java
@@ -0,0 +1,10 @@
+package aplicacaogit;
+
+public class AplicacaoGIt {
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa "
+                + "versionado com git e hospedado no GitHub!");
+    }
+    
+}
